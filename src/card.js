75a62003c0aaebed3a6847d3326ef69903af7ebf
David Eriksson <davideriksson@swedenmail.com>
2017-08-27 09:29:53 +0200

    Working init state

--- src/card.js
+++ src/card.js
@@ -1,32 +1,33 @@
 import React from "react"
+import classNames from "class-names"
+import "./card.css"
 
 export default class Card extends React.Component {
-	constructor(props) {
-		super(props)
-		this.real_pici = props.card.src
-		this.hidden_pici = "/images/flippy.jpg"
-		
-		this.state = {
-			flippy: false,
-		}
-	}
-	flippyflippy() {
-		this.setState({flippy: !this.state.flippy})
-		console.log("Hejhej " + this.state.flippy)
-		this.props.hoy(this.props.card, (unflippyCards) => {
-			unflippyCards.forEach(card => {card.unflip()})
-		})
-	}
-	unflip() {
-		this.setState({flippy: false})
-	}
-	render() {
-		return (
-			<div>
-				{
-					<img onClick={this.flippyflippy.bind(this)} src={this.state.flippy ? this.real_pici : this.hidden_pici} alt="nowayjose" />
-				}
-			</div>
-		)
-	}
+  state = {
+    flipped: false
+  }
+
+  handleClick = () => {
+    if (this.props.canFlip) {
+      this.setState({ flipped: true })
+      this.props.onFlip(this.props.photo, this.handleUnflipRequest)
+    }
+  }
+
+  handleUnflipRequest = () => {
+    this.setState({ flipped: false })
+  }
+
+  render() {
+    return (
+      <div
+        className="card"
+        onClick={this.handleClick}>
+        <div
+          className={classNames("image", { flipped: this.state.flipped })}
+          style={{ backgroundImage: `url(${this.props.photo})` }} />
+      </div>
+    )
+  }
+
 }
--- src/game.js
+++ src/game.js
@@ -1,58 +1,56 @@
 import React from "react"
 import Card from "./card"
-
-
+import shuffle from "./shuffle"
 
 const photos = [
-	"/images/dog-1.jpg",
-	"/images/dog-2.jpg",
-	"/images/dog-3.jpg",
-	"/images/dog-4.jpg",
-	"/images/dog-5.jpg",
-	"/images/dog-6.jpg",
-	"/images/awesome.jpg"
+  "/images/dog-1.jpg",
+  "/images/dog-2.jpg",
+  "/images/dog-3.jpg",
+  "/images/dog-4.jpg",
+  "/images/dog-5.jpg"
 ]
 
 export default class Game extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			cards: this.shuffle(photos),
-			flippedCards: []
-		}
-	}
-	shuffle(a) {
-		a = a.concat(a)
-    	for (let i = a.length; i; i--) {
-        	let j = Math.floor(Math.random() * i);
-        	[a[i - 1], a[j]] = [a[j], a[i - 1]];
-    	}
-    	return a.map(i => ({id: i + Math.random(), src: i}))
-	}
-	hoy = (card, unflippyflip) => {
-		this.state.flippedCards.push(card)
-		if(this.state.flippedCards.length === 2) {
-			if(this.state.flippedCards[0].src === this.state.flippedCards[1].src) {
-				alert("yay: " + this.state.flippedCards[0].src + ":" + card.src)
-				let supercards = this.state.cards.filter((item) => item.src !== card.src)
-				this.setState({cards: supercards})
-			} else  {
-				unflippyflip(this.state.flippedCards)
-				//this.setState({flippedCards: []})
-			}
-		}
-	}
-	render() {
-		return (
-			<div>
-				{
-					this.state.cards.map(
-						card => (
-							<Card hoy={this.hoy} card={card} key={card.id}/>
-							)
-					)
-				}
-			</div>
-		)
-	}
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards: this.duplicatedAndShuffledCards(),
+      flippedCards: []
+    }
+  }
+
+  duplicatedAndShuffledCards = () => (
+    shuffle([...photos, ...photos])
+  )
+
+  handleCardFlip = (photo, unflipCallback) => {
+    const flippedCards = [...this.state.flippedCards, { photo, unflipCallback }]
+    this.setState({ flippedCards }, this.handleFlippedCardChange)
+  }
+
+  handleFlippedCardChange = () => {
+    if (this.state.flippedCards.length === 2) {
+      setTimeout(() => {
+        this.state.flippedCards.forEach(card => {
+          card.unflipCallback()
+        })
+        this.setState({ flippedCards: [] })
+      }, 1000)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.cards.map(card => (
+          <Card
+            canFlip={this.state.flippedCards.length < 2}
+            onFlip={this.handleCardFlip}
+            photo={card} />
+        ))}
+      </div>
+    )
+  }
+
 }
