7193501d9ea6d34284f1e594c2e504f4cf2eb840
David Eriksson <davideriksson@swedenmail.com>
2017-08-27 15:22:35 +0200

    Added beforeEach function to game-store.test

--- src/stores/game-store.test.js
+++ src/stores/game-store.test.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import GameStore from './game-store'
 
 describe('Hejhej', function() {
+  var gameStore
+  beforeEach(function() {
+  	gameStore = new GameStore()
+  })
   it('duplicatedAndShuffledCards should return 10 results', function() {
-  	const gameStore = new GameStore()
   	const result = gameStore.duplicatedAndShuffledCards()
   	expect(result.length).toEqual(10)
   });
