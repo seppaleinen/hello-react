9555b93f6b04b9ad8e953447c8b28ba738a42bc7
David Eriksson <davideriksson@swedenmail.com>
2017-08-27 14:55:14 +0200

    Tests are more or less working again

--- src/card.test.js
+++ src/card.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './card';
+import GameStore from './stores/game-store'
+import { Provider } from "mobx-react"
+
+const stores  = {
+  gameStore: new GameStore()
+}
+
+const App = () => 
+  <Provider {...stores}>
+    <Card card={new Card("/images/dog-1.jpg")}/>
+  </Provider>
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Card src="/images/dog-1.jpg" />, div);
+  ReactDOM.render(<App />, div);
 });
--- src/game.test.js
+++ src/game.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './game';
+import GameStore from './stores/game-store'
+import { Provider } from "mobx-react"
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
-});
+const stores  = {
+  gameStore: new GameStore()
+}
+
+const App = () => 
+  <Provider {...stores}>
+    <Game />
+  </Provider>
+
+
+describe("TodoList", function() {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+})
+
