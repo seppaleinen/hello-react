706c2b00238b686ee78d28f321cb864d8968494b
David Eriksson <davideriksson@swedenmail.com>
2017-08-27 16:54:53 +0200

    Added some more tests to gamestore

--- src/stores/game-store.test.js
+++ src/stores/game-store.test.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from '../card'
 import GameStore from './game-store'
 
-describe('Hejhej', function() {
+describe('GameStore tests', function() {
   var gameStore
   beforeEach(function() {
   	gameStore = new GameStore()
   })
-  it('duplicatedAndShuffledCards should return 10 results', function() {
+  it('duplicatedAndShuffledCards should return 10 results', () => {
   	const result = gameStore.duplicatedAndShuffledCards()
   	expect(result.length).toEqual(10)
-  });
+  })
+  it('Constructor should set correct values', () => {
+    expect(gameStore.cards.length).toEqual(10)
+    expect(gameStore.flippedCards.length).toEqual(0)
+    expect(gameStore.flipCount).toEqual(0)
+  })
+  it('Attributes needs to be editable', () => {
+  	gameStore.cards.pop()
+  	gameStore.flippedCards.push(new Card(""))
+  	gameStore.flipCount = 1
+    expect(gameStore.cards.length).toEqual(9)
+    expect(gameStore.flippedCards.length).toEqual(1)
+    expect(gameStore.flipCount).toEqual(1)
+  })
+  it('Attributes should be reset after gameStore.reset', () => {
+  	gameStore.cards.pop()
+  	gameStore.flippedCards.push(new Card(""))
+  	gameStore.flipCount = 1
+  	gameStore.restart()
+    expect(gameStore.cards.length).toEqual(10)
+    expect(gameStore.flippedCards.length).toEqual(0)
+    expect(gameStore.flipCount).toEqual(0)
+  })
 })
