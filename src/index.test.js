a7fe42b2348ac18723d1763822a02f9e2e2c5392
David Eriksson <davideriksson@swedenmail.com>
2017-08-27 18:47:37 +0200

    small fix in index.test

--- src/index.test.js
+++ src/index.test.js
@@ -5,17 +5,19 @@ import Game from './game';
 import GameStore from './stores/game-store'
 import { Provider } from "mobx-react"
 
-const stores  = {
-  gameStore: new GameStore()
-}
-
-const App = () => 
-  <Provider {...stores}>
-    <Game />
-  </Provider>
-
 
 describe("Testing index", function() {
+  var stores
+  var App
+  beforeEach(function() {
+  	stores = {
+      gameStore: new GameStore()
+    }
+    App = () => 
+      <Provider {...stores}>
+        <Game />
+      </Provider>  
+  })
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
